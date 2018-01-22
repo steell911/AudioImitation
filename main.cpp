502bfe9057fbff0d440d3fc5c14ccdfb00d6f7e3
steell911 <patryaev.alexander@ya.ru>
2018-01-22 17:35:35 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**************************************************************************
+** Qt Creator license header template
+**   Special keywords:  22.01.2018 2018
+**   Environment variables:
+**   To protect a percent sign, use '%'.
+**************************************************************************/
+//	date: 22.01.2018
+//	file: %{Cpp:License:FileName}
+//	class: %{Cpp:License:ClassName}
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
